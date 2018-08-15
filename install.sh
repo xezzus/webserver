3d1f88e96b4b1afbc988ae0ab6768b9eb01a8ecf
Buturlakin Andrey <xezzus@gmail.com>
2018-08-15 13:36:52 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#
+# INSTALLS
+#
+
+# update
+pacman -Syu --noconfirm
+
+# install packages
+pacman -S --noconfirm nginx vim postgresql redis composer 
+pacman -S --noconfirm php php-fpm php-gd php-intl php-pgsql php-sqlite php-xsl php-geoip php-redis
+
+#
+# CONFIGURE
+#
+
+# add user "webserver"
+useradd -m webserver
+
+# postgresql
+usermod -d /home/postgres postgres
+mkdir /home/postgres && chown -R postgres:postgres /home/postgres
+su - postgres -c "initdb --locale ru_RU.UTF-8 -E UTF8 -D '/home/postgres/data'"
